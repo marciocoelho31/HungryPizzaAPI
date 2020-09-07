5b13254c8351fa2b5380c95f16842c4faed67722
Marcio Coelho <marcio@caster.com.br>
2020-09-07 10:28:28 -0300

    Ajustes nos testes de integração

--- HungryPizzaAPI_Testes/Integracao/IntegrationTests.cs
+++ HungryPizzaAPI_Testes/Integracao/IntegrationTests.cs
@@ -1,9 +1,8 @@
 ﻿using HungryPizzaAPI;
+using HungryPizzaAPI.Models;
 using Microsoft.AspNetCore.Mvc.Testing;
-using System;
-using System.Collections.Generic;
+using Newtonsoft.Json;
 using System.Net;
-using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -20,6 +19,7 @@ namespace HungryPizzaAPI_Testes
 
         [Theory]
         [InlineData("/API/Clientes")]
+        [InlineData("/API/Clientes/Historico/teste")]
         [InlineData("/API/EnderecoEntrega")]
         [InlineData("/API/Pedidos")]
         [InlineData("/API/Pizzas")]
@@ -35,6 +35,180 @@ namespace HungryPizzaAPI_Testes
             response.EnsureSuccessStatusCode(); // Status Code 200-299
 
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task TestPostClienteAsync()
+        {
+            // Arrange
+            var request = new
+            {
+                Url = "/API/Clientes",
+                Body = new
+                {
+                    Nome = "Teste",
+                    Login = "LoginTeste",
+                    EnderecoEntregaId = 2
+                }
+            };
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.PostAsync(request.Url, ContentHelper.GetStringContent(request.Body));
+            var value = await response.Content.ReadAsStringAsync();
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+        }
+
+        [Fact]
+        public async Task TestPostEnderecoEntregaAsync()
+        {
+            // Arrange
+            var request = new
+            {
+                Url = "/API/EnderecoEntrega",
+                Body = new
+                {
+                    Endereco = "Rua A",
+                    Bairro = "Centro",
+                    Cidade = "Rio de Janeiro",
+                    Estado = "RJ",
+                    Cep = "20000-000"
+                }
+            };
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.PostAsync(request.Url, ContentHelper.GetStringContent(request.Body));
+            var value = await response.Content.ReadAsStringAsync();
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+        }
+
+        [Fact]
+        public async Task TestPostCriarPedidoSemCadastroAsync()
+        {
+            // Arrange
+            var request = new
+            {
+                Url = "/API/Pedidos/CriarPedido",
+                Body = new
+                {
+                    NomeCliente = "Joao",
+                    EnderecoEntrega = new {
+                        Endereco = "Rua XYZ",
+                        Bairro = "Vila Nova",
+                        Cidade = "São Paulo",
+                        Estado = "SP",
+                        Cep = "01000-001"
+                    }
+                }
+            };
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.PostAsync(request.Url, ContentHelper.GetStringContent(request.Body));
+            var value = await response.Content.ReadAsStringAsync();
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+        }
+
+        [Fact]
+        public async Task TestPostCriarPedidoClienteCadastradoAsync()
+        {
+            // Arrange
+            var request = new
+            {
+                Url = "/API/Pedidos/CriarPedido/loginteste"
+            };
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.PostAsync(request.Url, ContentHelper.GetStringContent(null));
+            var value = await response.Content.ReadAsStringAsync();
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+        }
+
+        [Fact]
+        public async Task TestPostInserirPizza1SaborEmPedidoAsync()
+        {
+            // Arrange
+            var request = new
+            {
+                Url = "/API/Pedidos/InserirPizza/6"
+            };
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.PostAsync(request.Url, ContentHelper.GetStringContent(null));
+            var value = await response.Content.ReadAsStringAsync();
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+        }
+
+        [Fact]
+        public async Task TestPostInserirPizza2SaboresEmPedidoAsync()
+        {
+            // Arrange
+            var request = new
+            {
+                Url = "/API/Pedidos/InserirPizza/6-7"
+            };
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.PostAsync(request.Url, ContentHelper.GetStringContent(null));
+            var value = await response.Content.ReadAsStringAsync();
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+        }
+
+        [Fact]
+        public async Task TestPatchFinalizarPedidoAsync()
+        {
+            // Arrange
+            var requestCriaPedido = new
+            {
+                Url = "/API/Pedidos/CriarPedido",
+                Body = new
+                {
+                    NomeCliente = "Joao",
+                    EnderecoEntrega = new
+                    {
+                        Endereco = "Rua XYZ",
+                        Bairro = "Vila Nova",
+                        Cidade = "São Paulo",
+                        Estado = "SP",
+                        Cep = "01000-001"
+                    }
+                }
+            };
+
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.PostAsync(requestCriaPedido.Url, ContentHelper.GetStringContent(requestCriaPedido.Body));
+            var valuePedidoCriado = await response.Content.ReadAsStringAsync();
+
+            Pedido resultado = JsonConvert.DeserializeObject<Pedido>(valuePedidoCriado);
+            var requestFinalizaPedido = new
+            {
+                Url = "/API/Pedidos/" + resultado.Id
+            };
+
+            var responseFinalizaPedido = await client.PatchAsync(requestFinalizaPedido.Url, 
+                ContentHelper.GetStringContent(null));
+            var value = await responseFinalizaPedido.Content.ReadAsStringAsync();
+
+            // Assert
+            response.EnsureSuccessStatusCode();
         }
 
     }
